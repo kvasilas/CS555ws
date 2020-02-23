c11ed957a40c3e1f72caa7c34780040f6f68e969
Justin DeTone <jmdetone@gmail.com>
2020-02-23 18:20:50 -0500

    JD Sprint 1: Added family_structure functions testing

---
+++ family_structure_test.py
@@ -0,0 +1,9 @@
+import family_structure
+
+def fiveLessBirthsTest(people, families):
+    if family_structure.fiveLessBirths(people, families) == "More than five siblings were born on the same day"
+        return "Error, five or more births"
+
+def fifteenLessSiblingsTest(families):
+    if family_structure.fifteenLessSiblings(families) == "A family contains more than fourteen siblings":
+        return "Error, fifteen or more siblings in a family"
