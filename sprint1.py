0f3d53cded3c1f1c06a37611a8effb6c9f0bab24
RTom216 <rtom@stevens.edu>
2020-02-23 22:52:40 -0500

    Updated code with test for 12 and 16

--- sprint1.py
+++ sprint1.py
@@ -1,19 +1,29 @@
 import test_age
 import tag_parse as tag
 import age
+import family_structure_test
 
-# #store all information
-# people, families = tag.read_file('./kvSprint1.ged')
-# people = age.store_ages(families, people)
 
-# #KV User Stories
-# print(test_age.test_birth_b4_marr('aa', people))
-# #print(test_age.test_birth_b4_death(people))
+#KV User Stories
+people, families = tag.read_file('./kvSprint1.ged')
+people = age.store_ages(families, people)
+print(test_age.test_birth_b4_marr('aa', people))
+print(test_age.test_birth_b4_death(people))
 
 #JT User Stories
 people, families = tag.read_file('./jt_sprint1.ged')
 people = age.store_ages(families, people)
 print(test_age.test_less_than_one_fifty('@I1@', people))
 print(test_age.test_marrige_after_fourteen('@I2@', people))
+
 #JD User Stories
+peopleJD, familiesJD = tag.read_file('./jdSprint1.ged')
+print(family_structure_test.fiveLessBirthsTest(peopleJD, familiesJD))
+print(family_structure_test.fifteenLessSiblingsTest(familiesJD))
+
 #RT User Stories
+people, families = tag.read_file('./rtSprint1.ged')
+people = age.store_ages(families, people)
+print(family_structure_test.parentsNotTooOldTest(people, '@F1@'))
+print(family_structure_test.maleLastNameTest(people, '@F1@'))
+
