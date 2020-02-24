bc8ec7c4b97e9b7bcf10c4bc18b318b8bd0c687c
Justin Trugman <jtrugman14@gmail.com>
2020-02-23 19:22:38 -0500

    debugging

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tag_parse as tag
 import age
 
-people, families = tag.read_file('./proj02test.ged')
-print(families)
+people, families = tag.read_file('./runme.ged')
+print(people)
 people = age.store_ages(families, people)
 
 age.check_birth_before_marr('xp', people)
