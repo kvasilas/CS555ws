8de6d36966af346d9d26d138225d563320294687
Justin DeTone <jmdetone@gmail.com>
2020-02-23 21:31:36 -0500

    JD Sprint 1

--- sprint1.py
+++ sprint1.py
@@ -1,13 +1,15 @@
 import test_age
 import tag_parse as tag
 import age
+import family_structure_test
 
 # #store all information
-# people, families = tag.read_file('./kvSprint1.ged')
-# people = age.store_ages(families, people)
 
-# #KV User Stories
-# print(test_age.test_birth_b4_marr('aa', people))
+
+#KV User Stories
+people, families = tag.read_file('./kvSprint1.ged')
+people = age.store_ages(families, people)
+print(test_age.test_birth_b4_marr('aa', people))
 # #print(test_age.test_birth_b4_death(people))
 
 #JT User Stories
@@ -15,5 +17,10 @@ people, families = tag.read_file('./jt_sprint1.ged')
 people = age.store_ages(families, people)
 print(test_age.test_less_than_one_fifty('@I1@', people))
 print(test_age.test_marrige_after_fourteen('@I2@', people))
+
 #JD User Stories
+peopleJD, familiesJD = tag.read_file('./jdSprint1.ged')
+print(family_structure_test.fiveLessBirthsTest(peopleJD, familiesJD))
+print(family_structure_test.fifteenLessSiblingsTest(familiesJD))
+
 #RT User Stories
