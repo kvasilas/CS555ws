1594f1d9caba08ad512497c97d3e836f619da69d
Justin Trugman <jtrugman14@gmail.com>
2020-03-08 20:54:08 -0400

    adjusted error codes

--- sprint2.py
+++ sprint2.py
@@ -2,7 +2,7 @@ import test_age
 import tag_parse as tag
 import age
 from family_structure_test import *
-
+from multiprocessing import Process
 
 #KV User Stories
 people, families = tag.read_file('./kvSprint2.ged')
@@ -11,14 +11,20 @@ print(test_age.test_mar_b4_death('xp', people))
 print(test_age.test_div_b4_death('xp', people))
 
 
-#JT User Stories
-people, families = tag.read_file('./kvSprint2.ged')
-people = age.store_ages(families, people)
-print(uniqueIndividualIDsTest(people))
-print(uniqueFamilyIDsTest(families))
-print(listDeceasedTest(people))
-
 #JD User Stories
 
 
 #RT User Stories
+
+#JT User Stories
+people, families = tag.read_file('./kvSprint2.ged')
+people = age.store_ages(families, people)
+print(listDeceasedTest(people))
+
+peopleFam, familiesFam = tag.read_file('./jt_sprint2Fam.ged')
+peopleFam = age.store_ages(familiesFam, peopleFam)
+print(uniqueFamilyIDsTest(familiesFam))
+
+peopleIndi, familiesIndi = tag.read_file('./jt_sprint2.ged')
+peopleIndi = age.store_ages(familiesIndi, peopleIndi)
+print(uniqueIndividualIDsTest(peopleIndi))
