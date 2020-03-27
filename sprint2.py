b41d272bf4a3a7424d28263f43903e8e81b7334f
Justin Trugman <jtrugman14@gmail.com>
2020-03-26 20:47:38 -0400

    docs: showing how to use pull request #3

--- sprint2.py
+++ sprint2.py
@@ -85,12 +85,14 @@ print(test_age.test_div_b4_death(people))
 
 
 #JD User Stories
+# hey guys I'm testing a pull request 
 people, families = tag.read_file('./jd_sprint2.ged')
 #people = age.store_ages(families, people)
 pretty_print_people(people)
 pretty_print_families(families, people)
 print(noChildMarryTest(families))
 print(test_age.test_date_before_current(people, families))
+
 
 #RT User Stories
 people, families = tag.read_file('./rtSprint2.ged')
