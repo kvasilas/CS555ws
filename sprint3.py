7cde484cc781121e87394643ddf1b13edeac82ce
Justin Trugman <jtrugman14@gmail.com>
2020-03-29 12:35:59 -0400

    adding test gedcom files

---
+++ sprint3.py
@@ -0,0 +1,17 @@
+import test_age
+import tag_parse as tag
+import age
+import family_structure_test
+
+
+# JT User Stories
+people, families = tag.read_file('./jt_list_recent_births.ged')
+people = age.store_ages(families, people)
+print(test_age.listRecentBirths(people))
+
+# run validate age last because it will exit out the program when an invalid age is entered
+# this is because there is no point in continuing with the rest of the program when the dates are invalid
+people, families = tag.read_file('./jt_validate_age.ged')
+people = age.store_ages(families, people)
+print(test_age.test_validateDates(people))
+
