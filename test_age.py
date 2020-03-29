2f10382f9e94d96e1738a5279a0d316af08f2cd1
Justin Trugman <jtrugman14@gmail.com>
2020-03-29 14:27:32 -0400

    1st travis test

--- test_age.py
+++ test_age.py
@@ -1,52 +1,13 @@
+import unittest
 from age import *
+from tag_parse import *
 
-def test_less_than_one_fifty(key, people):
-    if (less_than_one_fifty(key, people) == "Death Age Invalid") or (less_than_one_fifty(key, people) == "Current Age Invalid"):
-        return "test_less_than_one_fifty passed"
+class TestAge(unittest.TestCase):
 
-def test_marrige_after_fourteen(key, people):
-    if (marrige_after_fourteen(key,people) == "Marrige under the age of 14 is invalid"):
-        return "marrige_after_fourteen passed"
+    def test_age_less_than_150(self):
+        people, families = read_file('./sprint1/jt_sprint1.ged')
+        people = store_ages(families, people)
+        self.assertEqual(less_than_one_fifty('@I1@', people), 'Death Age Invalid')
 
-def test_birth_b4_marr(key, people):
-    ans = check_birth_before_marr(key,people)
-    if("ERROR" in ans):
-        return(ans)
-    else:
-        return("test_birth_b4_marr passed")
-
-def test_birth_b4_death(people):
-    for key in people:
-        if('BIRT' in people[key].keys()):
-            if('Death' in people[key].keys()):
-                if('AGE' in people[key].keys()):
-                    if(people[key]['AGE'] < 0):
-                        yield(key+"ERROR, Birth Before Death")
-                    else:
-                        yield(key+"PASS Birth Before Death")
-
-
-def test_mar_b4_death(people):
-    for key in people:
-        output = mar_b4_death(key, people)
-        if("ERROR" in output):
-            return output
-
-
-def test_div_b4_death(people):
-    for key in people:
-        output = div_b4_death(key, people)
-        if("ERROR" in output):
-            return output
-
-def test_date_before_current(people, families):
-    result = datesBeforeCurrent(people, families)
-    if "ERROR" in result:
-        print(result)
-
-def test_list_recent_births(people):
-    return listRecentBirths(people)
-
-
-def test_validateDates(people):
-    return calc_ages(people)
+if __name__ == '__main__':
+    unittest.main()
